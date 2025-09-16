58e10f879855e065df92e320a9c706c294b79ea5
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-16 17:23:52 +0600

    k

---
+++ p6.py
@@ -0,0 +1,29 @@
+# return data type
+
+num=290
+
+print(type(num))
+
+num=3.4
+
+print(type(num))
+
+num="Mahim Ahmad"
+
+print(type(num))
+
+print(type(True))
+
+#string formatting
+
+
+num1=20
+num2=25
+
+print(f"{num1} + {num2} = {num1+num2}")
+
+
+#new line remove
+
+print("mahim ahmad",end=" ")
+print("01960603846")
