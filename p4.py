296683e7880b28f0a9f4d686e5bc16865a42b92b
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-16 16:29:37 +0600

    m

---
+++ p4.py
@@ -0,0 +1,14 @@
+num1=float(input("Enter base = "))
+num2=float(input("enter hight = "))
+
+result=0.5*num1*num2
+
+print(result)
+
+result=0.5*(num1+num2)
+
+print(result)
+
+result=3.1416*num1*num1
+
+print(result)
