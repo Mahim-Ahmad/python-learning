537b3056794ffe4f0b76ad7dcc7533f43a3c2b3d
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-16 18:46:21 +0600

    operation

---
+++ p7.py
@@ -0,0 +1,17 @@
+#relational operator
+
+result=30>20
+print(result)
+result=30<20
+print(result)
+print(30>=20)
+print(30<=20)
+print(30==20)
+print(30!=20)
+print(20==20)
+print("mahim"=="mahim")
+print("mahim"!="mahim")
+print("mahim"=="mahi")
+
+
+
