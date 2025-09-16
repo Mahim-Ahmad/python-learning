fa8fff74c7365b88f29476df08cbac35c47de5a4
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-17 01:50:25 +0600

    loop

--- p13.py
+++ p13.py
@@ -1,3 +1,17 @@
-#rang function 
-number = list(range(10))
-print(number)
+#while loop
+
+num=[2,4,1,5,8,9]
+
+n=len(num)
+i=0
+while i<n:
+    print(num[i])
+    i=i+1
+
+#for loop
+
+num=[2,4,1,5,8,9]
+
+for x in num:
+    print(x)
+ 
