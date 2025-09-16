b389637dd2e1805a6f7f714ec835fce8cf34e944
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-16 19:55:45 +0600

    updated

--- p8.py
+++ p8.py
@@ -9,4 +9,18 @@ elif mark%2==0:
     print("even")
 else:
     print("odd")
-    
+
+
+
+name=input("enter number : ")
+pas=input("enter pass : ")
+
+
+if "mahim"==name:
+    if("123"==pas):
+        print("successfull login")
+    else:
+        print("wrong pass")
+
+else:
+        print("try again")
