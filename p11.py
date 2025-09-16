7769a1ed44e8c1ebd3ac44c389783f4666466777
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-17 00:38:44 +0600

    list

---
+++ p11.py
@@ -0,0 +1,25 @@
+#list
+
+name = ["mahim","tamim","sagor","masud","tayeb"]
+print(name)
+print(name[0])
+
+print(name[2])
+
+print(name[4])
+
+print(name[2:]) #index 2 to last
+
+print(name[-1])   # - revers print
+
+#add list 
+
+print(name + ["khaled","mahin"])
+print(name * 3)
+
+#using operator in/not in
+
+print("mursed" in name)  #return true and false in list
+print("mursed" not in name)
+
+
