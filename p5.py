144a557109f36eaef39df67e9cbaa1c6f2397e8a
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-16 16:46:36 +0600

    math

---
+++ p5.py
@@ -0,0 +1,13 @@
+#math function
+
+from math import *
+
+print(max(10,20,30,10,20,33,93,9))
+print(min(10,20,30,10,20,33,93,9))
+print(abs(-4))
+print(pow(4,2))
+print(sqrt(25))
+print(round(3.4))
+print(round(3.8))
+print(floor(3.7))
+print(ceil(3.7))
