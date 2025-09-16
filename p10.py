10a0652456e578c5e17696e9232b55afa4c1da5d
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-17 00:11:34 +0600

    m

--- p10.py
+++ p10.py
@@ -6,3 +6,22 @@ while i<=10:
     i=i+1
 print("\nEnd")
 
+
+#break and continue
+
+i=1
+while i<=10: 
+    print(i,end=" ")
+    i=i+1
+    if i==5:
+        break
+print("\nEnd")
+
+i=1
+while i<=10:
+    if i==5:
+        i=i+1
+        continue
+    print(i,end=" ")
+    i=i+1
+print("\nEnd")
