ee765f28a75e968e84fb76a5ce852b7dd1e60d33
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-16 14:22:25 +0600

    p

--- p3.py
+++ p3.py
@@ -16,3 +16,22 @@ print("Sum is : ",mlty)
 div =int(num1) / int(num2)
 
 print("Sum is : ",div)
+
+num3=int(input("Enter First Number : "))
+num4=int(input("Enter second Number : "))
+
+sum =num3+ num4
+
+print("Sum is : ",sum)
+
+sub =num3- num4
+
+print("Sum is : ",sub)
+
+mlty =num3 * num4
+
+print("Sum is : ",mlty)
+
+div =num3 / num4
+
+print("Sum is : ",div)
