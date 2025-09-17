45925b1e06e36ef742023c6198b4c51ed5ee6720
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-18 02:50:37 +0600

    game

---
+++ p16.py
@@ -0,0 +1,12 @@
+#guessing game
+import random
+for x in range(1,5):
+    n=int(input("Enter you guess number: "))
+    
+    randomnumber=random.randint(1,10)
+    
+    if n==randomnumber :
+        print("congress")
+    else:
+        print("try again")
+        print(randomnumber)
