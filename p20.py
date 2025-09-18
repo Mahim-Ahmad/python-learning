b26d442622d7b132baa8df9f7a6d83861feaad09
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-19 02:41:10 +0600

    t

---
+++ p20.py
@@ -0,0 +1,29 @@
+#tuples
+
+student=(
+"mahim",
+"tamim",
+"kalam",
+"masud",
+
+)
+
+# student[o]="zahid"   change kora jai nah tuples a.
+
+print(student)
+print(student[0])
+
+
+student=(
+("mahim",23,3.77),
+"tamim",
+"kalam",
+"masud",
+
+)
+
+# student[o]="zahid"   change kora jai nah tuples a.
+
+print(student)
+print(student[0])
+print(student[1:])#1 index to last print
