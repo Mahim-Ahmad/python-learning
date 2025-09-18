a1621a570286d2ac98d85fa8b4688c7b5319885c
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-19 02:52:25 +0600

    set

---
+++ p21.py
@@ -0,0 +1,36 @@
+#set
+
+number = {1,2,3,4,5} #same valu print one time like 5 two in set but print one time
+
+print(number)
+
+num=set([1,2,3,4])
+print(num)
+
+#add number
+
+num.add(7)
+
+print(num)
+
+#remove number
+
+num.remove(3)
+print(num)
+
+#scarch number in set
+
+print(7 in num)
+print(7 in num)
+
+#set operation union,intersection like....
+
+num1={1,2,3,4}
+num2={3,4,5,6}
+
+print(num1 | num2)  #union
+print(num1 & num2)  #intercation
+print(num1 - num2)  #difference
+
+
+
