66be37bf3590d293755d2d25079a32fec789b55e
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-23 13:28:30 +0600

    xxargsss

--- p25.py
+++ p25.py
@@ -1,3 +1,34 @@
-def add(x,y):
-    sum=x+y
-    
+# xargs and xxargs
+
+#xargs
+
+def details(*x):
+    print(x)
+    print(x[0])
+    print(x[1:])
+
+
+details(1,"mahim",242)
+details(2,"tamim",242)
+
+
+def number(*num):
+    sum=0
+    for x in num:
+        sum=sum+x
+    print(sum)
+
+number(1,2)
+number(1,2,3)
+number(1,2,3,4)
+
+
+#xxargs
+
+def details(**student):
+    print(student)
+    print(student["id"])
+
+
+details(id=101,name="mahim")
+
