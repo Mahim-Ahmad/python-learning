259211c199fad1c10fc4e168253c2281b3b16034
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-23 17:31:42 +0600

    up

--- p30.py
+++ p30.py
@@ -1,5 +1,5 @@
 #Exception .....Error thik kora
-"""
+
 try:
     list=[20,0,10]
     result=list[0]/list[3]
@@ -9,11 +9,9 @@ except ZeroDivisionError:
     print("dividing by zero is not possible")
 except IndexError:
     print("index error")
-except (ValueError,ZeroDivisionError,IndexError):
-    print("somthing error")
-
 finally:
     print("successfully")
 
-"""
-
+# or this formate
+#except (ValueError,ZeroDivisionError,IndexError):
+    #print("somthing error")
