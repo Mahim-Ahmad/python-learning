23c423851f3bd8413e6465aea09720c092dd4a4c
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-25 16:28:35 +0600

    up

--- p43.py
+++ p43.py
@@ -53,3 +53,15 @@ pattern=r"a{1,3}$"#a use 1 to 3 time
 if re.match(pattern,"icecream"):
     print("match")
 
+#character class
+
+pattern=r"[aeiou]"#match in if vawle
+
+if re.match(pattern,"icecream"):
+    print("match")
+
+
+pattern=r"[A--Z][0-9]"#match in if range
+
+if re.match(pattern,"A7ecream"):
+    print("match")
