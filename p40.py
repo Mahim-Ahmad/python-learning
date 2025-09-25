7a1f0253298fa87284652de720638a1c08aa6d85
Mahim-Ahmad <mahim242-35-705@diu.edu.bd>
2025-09-25 14:51:46 +0600

    p

---
+++ p40.py
@@ -0,0 +1,15 @@
+#polymorphism....means ohurupi
+
+#built in po...  Fun...
+print(len("mahim"))
+print(len([10,20,30]))
+
+#user define p.... Fun
+
+def add(x,y,z=0):
+    return x+y+z
+
+print(add(2,3))
+print(add(2,3,4))
+
+#learn geeksfor geekss
